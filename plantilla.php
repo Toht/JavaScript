5e64b3d5423e4eea2f8b891e17227749dea934d2
iHans <hans.php@gmail.com>
2014-08-14 19:09:01 -0500

    Se agrega plantilla.

---
+++ plantilla.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Ejemplo de documento</title>
+<!-- Plantilla de documento HTML5 -->
+</head>
+
+<body>
+</body>
+</html>

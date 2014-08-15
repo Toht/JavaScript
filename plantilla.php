a24ba3de6c07ab65fd4335a06b81243840d90eb1
Aarón Toht <neon.ne@gmail.com>
2014-08-14 19:59:50 -0500

    Se actualizo plantilla

--- plantilla.php
+++ plantilla.php
@@ -1,11 +1,22 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Ejemplo de documento</title>
-<!-- Plantilla de documento HTML5 -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Ejemplo de documento</title>
+	<!-- Plantilla de documento HTML5 -->
+    <script type="text/javascript">
+    	alert("Hola mundo");
+    </script>
+    <script src="jsexterno.js" type="text/javascript"></script>
 </head>
 
 <body>
+<h1>Hola Hans</h1>
+<script type="text/javascript">
+alert("El cafec");
+</script>
+<?php
+echo "Esto es una prueba";
+?>
 </body>
 </html>
